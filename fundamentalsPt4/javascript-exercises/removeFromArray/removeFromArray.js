6ac4c0314cbe9e1ebc59a8d4c269aecded1a5202
NebulaBlaster <NebulaBlaster@mail.com>
2020-12-06 13:53:10 -0500

    complete removeFromArray exercise

--- fundamentalsPt4/javascript-exercises/removeFromArray/removeFromArray.js
+++ fundamentalsPt4/javascript-exercises/removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-// file has been modified
+const removeFromArray = function(newArray, ...valuesRemoved) {
+// newArray will be the user entered array that need to be modified. 
+// ...valuesRemoved will be a array of the values that need to be removed from newArray
+console.log(newArray);
+console.log(valuesRemoved);
+// have to loop through valesRemoved.length to access the values that need to be removed from newArray, in that loop we should be modifying newArray to be what it nees to be. 
+for(i = 0; valuesRemoved.length > i; i++ ){
+    let index = newArray.indexOf(valuesRemoved[i]);
+
+    if (index != -1){
+        newArray.splice(index,1);
+        console.log(newArray)
+    }
+}
+return newArray;
 }
 
 module.exports = removeFromArray
